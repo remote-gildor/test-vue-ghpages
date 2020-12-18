25b2d732a1e218865bbaf0004f9f0f7c32f557a4
Remote Gildor <73232606+remote-gildor@users.noreply.github.com>
2020-12-18 23:21:12 +0100

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <p>Sorry to crush your dreams if you thought otherwise.</p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Just testing lol</h1>
+
+    <p>Welcome to the test website on GitHub Pages.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>

f6a8a07e34a1a743aebf7c129d73919e25600cae
Remote Gildor <73232606+remote-gildor@users.noreply.github.com>
2020-12-18 23:50:57 +0100

    deploy

---
+++ js/about.645bf6d0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var r=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")]),n("p",[t._v("Sorry to crush your dreams if you thought otherwise.")])])}],u=n("2877"),a={},o=Object(u["a"])(a,r,s,!1,null,null,null);e["default"]=o.exports}}]);
+//# sourceMappingURL=about.645bf6d0.js.map
